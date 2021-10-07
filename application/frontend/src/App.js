1ac4dc2fdd2c80ccd4d31ec9df22efb372e0aead
Sanket Naik <sanketnaik99@gmail.com>
2021-10-07 16:17:09 -0700

    Search form updated with Search Preferences

--- application/frontend/src/App.js
+++ application/frontend/src/App.js
@@ -3,38 +3,100 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const App = () => {
+  // This stores and sets the search input field's value
   const [inputField, setInputField] = useState("");
+  // This stores and sets the pet select button's value.
+  const [petSelect, setPetSelect] = useState("");
+  // This stores and sets the rat select button's value.
+  const [smokingSelect, setSmokingSelect] = useState("");
+  // This stores and sets the room data value which is received from the Flask API.
   const [roomData, setRoomData] = useState([]);
 
-  const fetchData = async (query) => {
-    const data = await axios.post("http://localhost:5000/getRooms", {
-      description: query,
-    });
-    console.log(data.data);
+  // This function calls the `getRooms` API endpoint and passes the search query to
+  const fetchData = async () => {
+    // Regexp that only accepts numbers from 0-9
+    const zipRegex = "^[0-9]*$";
+    // This flag is used to pass either the location or zipCode based on the input.
+    let hasProvidedZipCode = false;
+
+    // Check if the inputField value provided is a ZipCode or a Location
+    if (inputField.match(zipRegex) && inputField !== "") {
+      hasProvidedZipCode = true;
+    }
+    // The QueryObject is passed as to the getRooms API endpoint.
+    const queryObj = {
+      ...(hasProvidedZipCode === true && { zipcode: inputField }),
+      ...(!hasProvidedZipCode && { location: inputField }),
+      ...(petSelect !== "" && { pets: petSelect }),
+      ...(smokingSelect !== "" && { smoking: smokingSelect }),
+    };
+    const data = await axios.post(
+      "http://127.0.0.1:5000/api/getRooms",
+      queryObj
+    );
+    // console.log(data.data);
     setRoomData(data.data);
   };
 
   // Handle Form submission and pass the data to the fetchData request.
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    fetchData(inputField);
+    fetchData();
   };
 
   // This function will run when the page loads - It should contain the initial fetch request which loads all the listings.
   useEffect(() => {
-    fetchData("Appliances");
+    fetchData();
   }, []);
 
   return (
     <div className="">
+      <div className="title-row">
+        <h1 className="title-app">GatorRoomer</h1>
+        <h2 className="title-class">CSC 648/848 Section 2</h2>
+        <h3 className="title-team">Team 04</h3>
+      </div>
       <div className="search-row">
         <h3 className="search-title">Search</h3>
-        <form onSubmit={(event) => handleFormSubmit(event)}>
-          <input
-            type="text"
-            onChange={(event) => setInputField(event.target.value)}
-            value={inputField}
-          />
+        <form
+          onSubmit={(event) => handleFormSubmit(event)}
+          className="form-container"
+        >
+          <div className="input-container">
+            <label className="input-label">
+              Search for a Location or Zip Code{" "}
+            </label>
+            <input
+              className="search-input"
+              type="text"
+              onChange={(event) => setInputField(event.target.value)}
+              value={inputField}
+            />
+          </div>
+          <div className="input-container">
+            <label className="input-label">Pet Preference</label>
+            <select
+              className="search-select"
+              value={petSelect}
+              onChange={(event) => setPetSelect(event.target.value)}
+            >
+              <option value="">None</option>
+              <option value="no pets">No Pets</option>
+              <option value="yes pets">Pet Friendly</option>
+            </select>
+          </div>
+          <div className="input-container">
+            <label className="input-label">Smoking Preference</label>
+            <select
+              className="search-select"
+              value={smokingSelect}
+              onChange={(event) => setSmokingSelect(event.target.value)}
+            >
+              <option value="">None</option>
+              <option value="no smoking">No Smoking</option>
+              <option value="yes smoking">Smoking Friendly</option>
+            </select>
+          </div>
           <button type="submit" className="submit-button">
             Submit
           </button>
