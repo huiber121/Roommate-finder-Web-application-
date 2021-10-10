feab8b8b1af05916496f5b2ca7e0e71eb4270d5f
swethac-1130 <>
2021-10-09 20:55:35 -0700

    db.py update

--- application/backend/database.py
+++ application/backend/database.py
@@ -4,32 +4,33 @@ from mysql.connector import pooling
 
 
 class Database:
+
     '''This class is used to connect to DB from connections in the pool & grt the data'''
 
     connection_pool = pooling.MySQLConnectionPool(
-                pool_name='db_pool',
-                pool_size=10,
-                pool_reset_session=True,
-                host='group4-db.czpwr5igmvey.us-west-1.rds.amazonaws.com'
-                    ,
-                database='Test1',
-                user='admin',
-                password='admin1234',
-                )
-    # Get connection object from a pool            
-    connection_object = connection_pool.get_connection()            
+        pool_name='db_pool',
+        pool_size=10,
+        pool_reset_session=True,
+        host='group4-db.czpwr5igmvey.us-west-1.rds.amazonaws.com',
+        database='Test1',
+        user='admin',
+        password='admin1234',
+        )
+
+    # Get connection object from a pool
+
+    connection_object = connection_pool.get_connection()
 
     def get_data(self, sql):
         """Gets data from DB using connection object from connection pool."""
-        try:   
+
+        try:
             if self.connection_object.is_connected():
                 cursor = self.connection_object.cursor()
                 cursor.execute(sql)
                 record = cursor.fetchall()
-                #print ('Data from db ', record)
                 return record
         except Error as error:
             print ('Error while connecting to MySQL using Connection pool '
                    , error)
             return None
-        
