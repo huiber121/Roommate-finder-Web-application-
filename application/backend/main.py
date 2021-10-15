bd2d7a73fcc5f13a011778fba163cd6bfe0b958c
swethac-1130 <>
2021-10-15 11:35:45 -0700

    roommate search

--- application/backend/main.py
+++ application/backend/main.py
@@ -1,29 +1,43 @@
-"""Flask module"""
+"""Importing necessary module"""
+import sys
+import logging
+import os.path
 from inspect import currentframe
 from inspect import getframeinfo
 from flask import Flask
 from flask_cors import CORS
-import rooms
-import logging
+CONTROLLER_DIR = \
+    os.path.abspath(os.path.join(os.path.dirname(__file__))) \
+    + '/controller/'
+sys.path.append(CONTROLLER_DIR)
+sys.dont_write_bytecode = True
+import roomscontroller
+import userscontroller
+
 
 APP = Flask(__name__)  # name of the module
 APP.config['CORS_HEADERS'] = 'Content-Type'
-APP.config['SESSION_PERMANENT'] = False
-APP.config['SESSION_TYPE'] = 'filesystem'
 CORS(APP, resources={r"*": {'origins': r"*"}})
-frameinfo = getframeinfo(currentframe())
+FRAOMEINFO = getframeinfo(currentframe())
 LOGGER = logging.getLogger(__name__)
 
 
 @APP.route('/api/getRooms', methods=['POST'])
 def get_rooms():
     """Endpoint to get all rooms for the given search conditions."""
-
     LOGGER.info(' Inside /api/getRooms')
-    rooms_list = rooms.get_all_rooms()
+    rooms_list = roomscontroller.get_all_rooms()
     return rooms_list
 
+
+@APP.route('/api/getRoommates', methods=['POST'])
+def get_roommates():
+    """Endpoint to get all rooms for the given search conditions."""
+    LOGGER.info('Inside /api/getRoommates')
+    roommates_list = userscontroller.get_all_roommates()
+    return roommates_list
+
 if __name__ == '__main__':
-    logging.basicConfig(filename='backend.log', level=logging.ERROR)
+    logging.basicConfig(filename='backend.log', level=logging.INFO)
     APP.run('0.0.0.0', port=5000)
     APP.run(debug=True)
