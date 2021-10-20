1a2b89a9e3e21675aec3807fb509cbd02ee62d84
Sanket Naik <sanketnaik99@gmail.com>
2021-10-19 19:01:44 -0700

    Login Page and Navbar updated.

--- application/frontend/src/pages/auth/login.js
+++ application/frontend/src/pages/auth/login.js
@@ -1,40 +1,105 @@
+import { Field, Form, Formik } from "formik";
 import React from "react";
+import { string } from "yup";
+import { object } from "yup";
+import "./login.css";
 
 const Login = () => {
-  return (
-    <div>
-      <div class="card">
-        <div class="card-image">
-          <figure class="image is-4by3">
-            <img
-              src="https://bulma.io/images/placeholders/1280x960.png"
-              alt="Placeholder image"
-            />
-          </figure>
-        </div>
-        <div class="card-content">
-          <div class="media">
-            <div class="media-left">
-              <figure class="image is-48x48">
-                <img
-                  src="https://bulma.io/images/placeholders/96x96.png"
-                  alt="Placeholder image"
-                />
-              </figure>
-            </div>
-            <div class="media-content">
-              <p class="title is-4">John Smith</p>
-              <p class="subtitle is-6">@johnsmith</p>
-            </div>
-          </div>
+  const loginValidationSchema = object().shape({
+    loginid: string()
+      .required("The Login ID is required.")
+      .min(2, "Please enter a valid Login ID.")
+      .max(50, "Please enter a valid Login ID."),
+    password: string()
+      .required("The password is required.")
+      .min(2, "Please enter a valid password.")
+      .max(50, "Please enter a valid password."),
+  });
 
-          <div class="content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            nec iaculis mauris. <a>@bulmaio</a>.<a href="#">#css</a>{" "}
-            <a href="#">#responsive</a>
-            <br />
-            <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-          </div>
+  return (
+    <div className="login-root-container">
+      <div className="columns is-mobile is-centered is-gapless">
+        <div className="column is-11-mobile is-three-quarters-tablet is-half-desktop has-text-centered">
+          <h1 className="is-size-1 has-text-weight-bold">Login</h1>
+          <h5 className="is-size-5 has-text-weight-bold">
+            Fill out the form below to login to the application.
+          </h5>
+        </div>
+      </div>
+      <div className="columns is-mobile is-centered is-gapless">
+        <div className="column is-11-mobile is-three-quarters-tablet is-half-desktop has-text-centered ">
+          <Formik
+            initialValues={{ loginid: "", password: "" }}
+            validationSchema={loginValidationSchema}
+            onSubmit={(values) => {
+              console.log(values);
+            }}
+          >
+            {({ errors, touched }) => (
+              <Form>
+                <div className="field is-horizontal">
+                  <div className="field-label is-normal">
+                    <label className="label">Login ID</label>
+                  </div>
+                  <div className="field-body">
+                    <div className="field">
+                      <p className="control">
+                        <Field
+                          className="input is-normal"
+                          name="loginid"
+                          placeholder="Enter your Login ID"
+                        />
+                      </p>
+                      {errors.loginid && touched.loginid ? (
+                        <div className="has-text-danger has-text-weight-semibold">
+                          {errors.loginid}
+                        </div>
+                      ) : null}
+                    </div>
+                  </div>
+                </div>
+                <div className="field is-horizontal">
+                  <div className="field-label is-normal">
+                    <label className="label">Password</label>
+                  </div>
+                  <div className="field-body">
+                    <div className="field">
+                      <p className="control">
+                        <Field
+                          className="input is-normal"
+                          name="password"
+                          placeholder="Enter your Password"
+                          type="password"
+                        />
+                      </p>
+                      {errors.password && touched.password ? (
+                        <div className="has-text-danger has-text-weight-semibold">
+                          {errors.password}
+                        </div>
+                      ) : null}
+                    </div>
+                  </div>
+                </div>
+                <button className="button is-link is-medium">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="submit-icon"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                  Submit
+                </button>
+              </Form>
+            )}
+          </Formik>
         </div>
       </div>
     </div>
