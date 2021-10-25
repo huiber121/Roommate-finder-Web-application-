79583f4d291e326224d026832e45470d13143fb5
william zhong <wzhong@mail.sfsu.edu>
2021-10-25 16:51:45 -0700

    registration page update

--- application/frontend/src/pages/auth/register.js
+++ application/frontend/src/pages/auth/register.js
@@ -23,7 +23,36 @@ const Register = () => {
     .required('Password confirmation is required.')
     .oneOf([Yup.ref('password'),null], 'Password must match'),
     terms: Yup.boolean()
-    .oneOf([true],"Must Accept Terms and Conditions")
+    .oneOf([true],"Must Accept Terms and Conditions"),
+    accountType: Yup
+    .number()
+    .nullable()
+    .required('Please indicate your account type.'),
+    firstName: Yup.string().required('Field is required'),
+    lastName: Yup.string().required('Field is required'),
+    middleName: Yup.string(),
+    gender: Yup.number()
+    .nullable()
+    .required('Please indicate your gender'),
+    ssn: Yup.string()
+      .matches(/^[0-9]*$/, "Numbers only.")
+      .min(7, "SSN must be 7 digits long.")
+      .max(7, "SSN must be 7 digits long.")
+      .required('Please enter your SSN.'),
+    school: Yup.string(),
+    gradeLevel: Yup.string(),
+    major: Yup.string()
+    .min(2, "Please enter a valid major.")
+    .max(50, "Please enter a valid major."),
+    location: Yup.string()
+    .min(2, "Please enter a valid loacation.")
+    .max(180, "Please enter a valid location.")
+    .required("Please indicate your location/job location"),
+    zipCode: Yup.string()
+    .matches(/^[0-9]+$/, "Must be only digits")
+    .min(5, 'Must be exactly 5 digits')
+    .max(5, 'Must be exactly 5 digits')
+    .required("Please enter your zip code.")
   });
 
   return (
@@ -39,7 +68,24 @@ const Register = () => {
       <div className="columns is-mobile is-centered is-gapless">
         <div className="column is-11-mobile is-three-quarters-tablet is-half-desktop has-text-centered ">
           <Formik 
-            initialValues={{ username: "", email: "", password: "", confirmPassword: "", terms: "" }}
+            initialValues={{ 
+              username: "", 
+              email: "", 
+              password: "", 
+              confirmPassword: "", 
+              terms: "", 
+              accountType: "", 
+              firstName:"", 
+              lastName: "",
+              middleName:"",
+              gender:"",
+              ssn:"",
+              school: "",
+              gradeLevel: "",
+              major: "",
+              location: "",
+              zipCode:""
+            }}
             validationSchema={registrationValidationSchema}
             onSubmit={(values) => {
               console.log(values);
@@ -125,35 +171,259 @@ const Register = () => {
                             type="password"
                           />
                         </p>
-                        {errors.confirmPassword && touched.confirmPassword ? (
+                        {errors.confirmPassword && touched.confirmPassword? (
                           <div className="has-text-danger has-text-weight-semibold">
                             {errors.confirmPassword}
                           </div>
                         ) : null}
                       </div>
-                    </div>
+                    </div>    
                   </div>
                   <div className="field is-horizontal">
-                      <div className="field-body">
-                        <div className="field">
-                          <p className="control">
-                            <Field
-                              className="form-check-label"
-                              name="terms"
-                              type="checkbox"
-                            />
-                            <label htmlFor="terms">
-                              I agree to the Terms and Conditions.  
-                            </label>
-                          </p>
-                          {errors.terms && touched.terms ? (
-                            <div className="has-text-danger has-text-weight-semibold">
-                              {errors.terms}
-                            </div>
-                          ) : null}
-                        </div>
-                      </div> 
-                    </div>   
+                    <div className="field-body">
+                      <div className="field">
+                        <p className="control">
+                          <label>Account Type
+                            <Field name="accountType" as="select">
+                              <option value={null}>Select a type</option>
+                              <option value={0}>student</option>
+                              <option value={1}>Professional</option>
+                              <option value={2}>Land Lord</option>
+                            </Field>
+                          </label>
+                        </p>
+                        {errors.accountType && touched.accountType ? (
+                          <div className="has-text-danger has-text-weight-semibold">
+                            {errors.accountType}
+                          </div>
+                        ) : null}
+                      </div>
+                    </div>    
+                  </div>
+                  <div className="field is-horizontal">
+                    <div className="field-label is-normal">
+                      <label className="label">First Name</label>
+                    </div>
+                    <div className="field-body">
+                      <div className="field">
+                        <p className="control">
+                          <Field
+                            className="input is-normal"
+                            name="firstName"
+                            placeholder="First Name"
+                          />
+                        </p>
+                        {errors.firstName && touched.firstName? (
+                          <div className="has-text-danger has-text-weight-semibold">
+                            {errors.firstName}
+                          </div>
+                        ) : null}
+                      </div>
+                      <div className="field-label is-normal">
+                      <label className="label">Middle Name</label>
+                      </div>
+                      <div className="field">
+                        <p className="control">
+                          <Field
+                            className="input is-normal"
+                            name="middleName"
+                            placeholder="Middle Name"
+                          />
+                        </p>
+                        {errors.middleName && touched.middleName? (
+                          <div className="has-text-danger has-text-weight-semibold">
+                            {errors.middleName}
+                          </div>
+                        ) : null}
+                      </div>
+                      <div className="field-label is-normal">
+                      <label className="label">Last Name</label>
+                      </div>
+                      <div className="field">
+                        <p className="control">
+                          <Field
+                            className="input is-normal"
+                            name="lastName"
+                            placeholder="Last Name"
+                          />
+                        </p>
+                        {errors.lastName && touched.lastName? (
+                          <div className="has-text-danger has-text-weight-semibold">
+                            {errors.lastName}
+                          </div>
+                        ) : null}
+                      </div>
+                    </div>    
+                  </div>
+                  <div className="field is-horizontal">
+                    <div className="field-body">
+                      <div className="field">
+                        <p className="control">
+                          <label>Gender
+                            <Field name="gender" as="select">
+                              <option value={null}>Select a type</option>
+                              <option value={0}>Male</option>
+                              <option value={1}>Female</option>
+                              <option value={2}>Other</option>
+                            </Field>
+                          </label>
+                        </p>
+                        {errors.gender && touched.gender ? (
+                          <div className="has-text-danger has-text-weight-semibold">
+                            {errors.gender}
+                          </div>
+                        ) : null}
+                      </div>
+                    </div>    
+                  </div>
+                  <div className="field is-horizontal">
+                    <div className="field-label is-normal">
+                      <label className="label">SSN</label>
+                    </div>
+                    <div className="field-body">
+                      <div className="field">
+                        <p className="control">
+                          <Field
+                            className="input is-normal"
+                            name="ssn"
+                            placeholder="Enter your SSN"
+                            type="password"
+                          />
+                        </p>
+                        {errors.ssn && touched.ssn? (
+                          <div className="has-text-danger has-text-weight-semibold">
+                            {errors.ssn}
+                          </div>
+                        ) : null}
+                      </div>
+                    </div>    
+                  </div>
+                  <div className="field is-horizontal">
+                    <div className="field-body">
+                      <div className="field">
+                        <p className="control">
+                          <label>School
+                            <Field name="school" as="select">
+                              <option value={null}>Select School</option>
+                              <option value={0}>Bueiness</option>
+                              <option value={1}>Education</option>
+                              <option value={2}>Ethnic study</option>
+                              <option value={3}>Health and Social Science</option>
+                              <option value={4}>Liberal and creative arts</option>
+                              <option value={5}>Science and Engineering</option>
+                            </Field>
+                          </label>
+                        </p>
+                        {errors.school && touched.school ? (
+                          <div className="has-text-danger has-text-weight-semibold">
+                            {errors.school}
+                          </div>
+                        ) : null}
+                      </div>
+                    </div>    
+                  </div>
+                  <div className="field is-horizontal">
+                    <div className="field-body">
+                      <div className="field">
+                        <p className="control">
+                          <label>Grade Level
+                            <Field name="gradeLevel" as="select">
+                              <option value={null}>Select Grade Level</option>
+                              <option value={0}>Undergraduate</option>
+                              <option value={1}>Graduate/Professional</option>
+                            </Field>
+                          </label>
+                        </p>
+                        {errors.gradeLevel && touched.gradeLevel ? (
+                          <div className="has-text-danger has-text-weight-semibold">
+                            {errors.gradeLevel}
+                          </div>
+                        ) : null}
+                      </div>
+                    </div>    
+                  </div>
+                  <div className="field is-horizontal">
+                    <div className="field-label is-normal">
+                      <label className="label">Major</label>
+                    </div>
+                    <div className="field-body">
+                      <div className="field">
+                        <p className="control">
+                          <Field
+                            className="input is-normal"
+                            name="major"
+                            placeholder="Enter your major"
+                          />
+                        </p>
+                        {errors.major && touched.major ? (
+                          <div className="has-text-danger has-text-weight-semibold">
+                            {errors.major}
+                          </div>
+                        ) : null}
+                      </div>
+                    </div>    
+                  </div>
+                  <div className="field is-horizontal">
+                    <div className="field-label is-normal">
+                      <label className="label">Location/Job Location</label>
+                    </div>
+                    <div className="field-body">
+                      <div className="field">
+                        <p className="control">
+                          <Field
+                            className="input is-normal"
+                            name="location"
+                            placeholder="Enter your location/job location"
+                          />
+                        </p>
+                        {errors.location && touched.location ? (
+                          <div className="has-text-danger has-text-weight-semibold">
+                            {errors.location}
+                          </div>
+                        ) : null}
+                      </div>
+                    </div>    
+                  </div>
+                  <div className="field is-horizontal">
+                    <div className="field-label is-normal">
+                      <label className="label">Zip Code</label>
+                    </div>
+                    <div className="field-body">
+                      <div className="field">
+                        <p className="control">
+                          <Field
+                            className="input is-normal"
+                            name="zipCode"
+                            placeholder="Enter your zipcode"
+                          />
+                        </p>
+                        {errors.zipCode && touched.zipCode ? (
+                          <div className="has-text-danger has-text-weight-semibold">
+                            {errors.zipCode}
+                          </div>
+                        ) : null}
+                      </div>
+                    </div>    
+                  </div>
+                  <div className="field is-horizontal">
+                    <div className="field-body">
+                      <div className="field">
+                        <p className="control">
+                          <Field
+                            type="checkbox"
+                            name="terms"
+                            className="form-check-input"
+                          />
+                          <label htmlFor="terms">I agree to the Terms and Condidtions.</label>
+                        </p>
+                        {errors.terms && touched.terms ? (
+                          <div className="has-text-danger has-text-weight-semibold">
+                            {errors.terms}
+                          </div>
+                        ) : null}
+                      </div>
+                    </div>
+                  </div>
                   <button className="button is-link is-medium">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
