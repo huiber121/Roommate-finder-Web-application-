c64d3b60120783d2d73fa140ccf2774b3f8569f7
swethac-1130 <>
2021-11-01 14:03:39 -0700

    pylint changes

--- application/backend/controller/profcontroller.py
+++ application/backend/controller/profcontroller.py
@@ -1,28 +1,26 @@
-import json
-import ast
-import sys
-import os.path
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Importing necessary modules"""
+
 import logging
-db_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.append(db_dir)
-sys.dont_write_bytecode = True
-from flask import render_template, request
 from db.database import Database
 
-
 LOGGER = logging.getLogger(__name__)
-dbinstance = Database()  
-        
+DBINSTANCE = Database()
+
 
 def add_prof(uijson):
-  countprofsql = "SELECT COUNT(*) FROM Test1.Professional_User;"
-  count = dbinstance.get_data(countprofsql)
-  print(count[0][0], type(count[0][0]))
-  pid = count[0][0] + 1
+    """This method is to add prof users."""
+    countprofsql = 'SELECT COUNT(*) FROM Test1.Professional_User;'
+    count = DBINSTANCE.get_data(countprofsql)
+    print (count[0][0], type(count[0][0]))
+    pid = count[0][0] + 1
 
-  profsql = "INSERT INTO `Test1`.`Professional_User` (`Prof_ID`, `JobLocation`, `Zipcode`) VALUES ('"+str(pid)+"', '"+uijson['joblocation']+"', '"+str(uijson['zipcode'])+"');"
-  LOGGER.info(" Add prof SQL "+ profsql)
-  result = dbinstance.add_data(profsql)
-  LOGGER.info(" Before returning add_prof "+result)
-  return result    
-
+    profsql = \
+        "INSERT INTO `Test1`.`Professional_User` (`Prof_ID`, `JobLocation`, `Zipcode`) VALUES ('" \
+        + str(pid) + "', '" + uijson['joblocation'] + "', '" \
+        + str(uijson['zipcode']) + "');"
+    LOGGER.info(' Add prof SQL {}'.format(profsql))
+    result = DBINSTANCE.add_data(profsql)
+    LOGGER.info(' Before returning add_prof {}'.format(result))
+    return result
